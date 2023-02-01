f8ed4ece10ab822c7eaac3eec8e622018ac73de8
Zbyněk Drápela <zbynek.drapela@gmail.com>
2023-02-01 17:11:24 +0100

    TODO Deploy test
    It is failing probably in second part of `deployKnativeServiceAndEventingBindings` Assume problem with environment.

---
+++ packages/kn-plugin-workflow/pkg/command/deploy_test.go
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2023 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package command
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strconv"
+	"testing"
+
+	"github.com/kiegroup/kie-tools/packages/kn-plugin-workflow/pkg/common"
+)
+
+type testDeploy struct {
+	input    DeployCmdConfig
+	expected bool
+}
+
+var testRunDeploySuccess = []testDeploy{
+	{input: DeployCmdConfig{Path: "./target/kubernetes"}, expected: true},
+}
+
+func fakeRunDeploy(testIndex int) func(command string, args ...string) *exec.Cmd {
+	return func(command string, args ...string) *exec.Cmd {
+		cs := []string{"-test.run=TestHelperRunDeploy", "--", command}
+		cs = append(cs, args...)
+		cmd := exec.Command(os.Args[0], cs...)
+		cmd.Env = []string{fmt.Sprintf("GO_TEST_HELPER_RUN_DEPLOY_IMAGE=%d", testIndex)}
+		return cmd
+	}
+}
+
+func TestHelperRunDeploy(t *testing.T) {
+	testIndex, err := strconv.Atoi(os.Getenv("GO_TEST_HELPER_RUN_DEPLOY_IMAGE"))
+	if err != nil {
+		return
+	}
+	fmt.Fprintf(os.Stdout, "%v", testRunDeploySuccess[testIndex].expected)
+	os.Exit(0)
+}
+
+func TestRunDeploy_Success(t *testing.T) {
+	for testIndex, test := range testRunDeploySuccess {
+		common.ExecCommand = fakeRunDeploy(testIndex)
+		defer func() { common.ExecCommand = exec.Command }()
+
+		out, err := deployKnativeServiceAndEventingBindings(test.input)
+		if err != nil {
+			t.Errorf("Expected nil error, got %#v", err)
+		}
+
+		if out != test.expected {
+			t.Errorf("Expected %v, got %v", test.expected, out)
+		}
+	}
+}
