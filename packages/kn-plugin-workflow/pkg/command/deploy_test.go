cc470143bc088e3de44afa0b429df2cb2d263704
Zbyněk Drápela <zbynek.drapela@gmail.com>
2023-02-08 17:33:31 +0100

    Add testing of file kogito.yml

--- packages/kn-plugin-workflow/pkg/command/deploy_test.go
+++ packages/kn-plugin-workflow/pkg/command/deploy_test.go
@@ -27,13 +27,14 @@ import (
 )
 
 type testDeploy struct {
-	input    DeployCmdConfig
-	expected bool
+	input      DeployCmdConfig
+	expected   bool
+	createFile string
 }
 
 var testRunDeploySuccess = []testDeploy{
-	{input: DeployCmdConfig{Path: "./target/kubernetes"}, expected: false},
-	{input: DeployCmdConfig{Path: ""}, expected: false},
+	{input: DeployCmdConfig{Path: "./target/kubernetes"}, expected: false, createFile: "kogito.yml"},
+	{input: DeployCmdConfig{Path: ""}, expected: false, createFile: ""},
 }
 
 func fakeRunDeploy(testIndex int) func(command string, args ...string) *exec.Cmd {
@@ -60,6 +61,11 @@ func TestRunDeploy_Success(t *testing.T) {
 		common.ExecCommand = fakeRunDeploy(testIndex)
 		defer func() { common.ExecCommand = exec.Command }()
 
+		if test.createFile != "" {
+			createFileInFolderStructure(t, test.createFile)
+			defer deleteFolderStructure(t)
+		}
+
 		out, err := deployKnativeServiceAndEventingBindings(test.input)
 		if err != nil {
 			t.Errorf("Expected nil error, got %#v", err)
@@ -69,4 +75,23 @@ func TestRunDeploy_Success(t *testing.T) {
 			t.Errorf("Expected %v, got %v", test.expected, out)
 		}
 	}
+}
+
+func deleteFolderStructure(t *testing.T) {
+	err := os.RemoveAll("./target")
+	if err != nil {
+		t.Error("Unable to delete folder structure")
+	}
+}
+
+func createFileInFolderStructure(t *testing.T, fileName string) {
+	err := os.MkdirAll("./target/kubernetes", 0750)
+	if err != nil {
+		t.Error("Unable to create folder structure")
+	}
+	file, err := os.Create("./target/kubernetes/" + fileName)
+	if err != nil {
+		t.Error("Unable to create" + fileName + "file")
+	}
+	defer file.Close()
 }
