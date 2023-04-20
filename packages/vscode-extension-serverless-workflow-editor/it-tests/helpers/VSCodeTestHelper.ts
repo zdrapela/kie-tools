e263e072698a61f5946bfc729d99bf26c4b4a165
Zbyněk Drápela <zbynek.drapela@gmail.com>
2023-04-20 11:57:24 +0200

    Revert "Longer wait time"
    This reverts commit 42dd2eb4663779b79f4446642592828886757a92.

--- packages/vscode-extension-serverless-workflow-editor/it-tests/helpers/VSCodeTestHelper.ts
+++ packages/vscode-extension-serverless-workflow-editor/it-tests/helpers/VSCodeTestHelper.ts
@@ -125,7 +125,7 @@ export default class VSCodeTestHelper {
         await fileItem.click();
       }
     }
-    await sleep(35000);
+    await sleep(5000);
 
     const editorGroups = await this.workbench.getEditorView().getEditorGroups();
     // should be always two groups, one text editor and one swf editor
