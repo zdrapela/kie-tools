a90af1f45eaac756bb0d56e800d5916992c2b851
Zbyněk Drápela <zbynek.drapela@gmail.com>
2023-05-16 13:32:54 +0200

    Improve test

--- packages/serverless-workflow-vscode-extension/it-tests/serverless-workflow-editor-extension-svg-filepath.test.ts
+++ packages/serverless-workflow-vscode-extension/it-tests/serverless-workflow-editor-extension-svg-filepath.test.ts
@@ -15,14 +15,21 @@
  */
 
 import * as path from "path";
+import * as fs from "fs";
 import { assert } from "chai";
 import { VSCodeTestHelper } from "@kie-tools/vscode-extension-common-test-helpers";
 import SwfEditorTestHelper from "./helpers/swf/SwfEditorTestHelper";
 import SwfTextEditorTestHelper from "./helpers/swf/SwfTextEditorTestHelper";
 
-describe("Serverless workflow editor - smoke integration tests", () => {
+describe.only("Serverless workflow editor - smoke integration tests", () => {
   const TEST_PROJECT_FOLDER: string = path.resolve("it-tests-tmp", "resources", "greeting-flow");
   const DIST_IT_TESTS_FOLDER: string = path.resolve("dist-it-tests");
+
+  const FILE_NAME_NO_EXTENSION = "greetings";
+  const WORKFLOW_NAME = `${FILE_NAME_NO_EXTENSION}.sw.json`;
+  const SVG_NAME = `${FILE_NAME_NO_EXTENSION}.svg`;
+  const RESOURCE_DIRECTORY_PATH = path.join("src", "main", "resources");
+  const SVG_FILE_PATH = path.resolve(TEST_PROJECT_FOLDER, RESOURCE_DIRECTORY_PATH, SVG_NAME);
 
   let testHelper: VSCodeTestHelper;
 
@@ -44,21 +51,17 @@ describe("Serverless workflow editor - smoke integration tests", () => {
     await testHelper.closeAllNotifications();
   });
 
-  it("Opens greetings.sw.json and loads two editor groups", async function () {
-    this.timeout(40000);
-    const editorWebviews = await testHelper.openFileFromSidebar("greetings.sw.json", "src/main/resources");
+  it("Opens greetings.sw.json, saves it, and verifies SVG generation", async function () {
+    this.timeout(20000);
+
+    const editorWebviews = await testHelper.openFileFromSidebar(WORKFLOW_NAME, RESOURCE_DIRECTORY_PATH);
 
     const swfEditor = new SwfEditorTestHelper(editorWebviews[1]);
     const swfTextEditor = new SwfTextEditorTestHelper(editorWebviews[0]);
 
     await testHelper.saveFileInTextEditor();
 
-    // find elements, this asserts they exist
-    await swfEditor.getWorkbenchPanelViewElement();
-    await swfEditor.getCanvasPanelElement();
-
-    // expect 7 nodes /w end and start
-    const nodeIds = await swfEditor.getAllNodeIds();
-    assert.equal(nodeIds.length, 7);
+    // verify SVG was generated after file save
+    assert(fs.existsSync(SVG_FILE_PATH), `SVG file was not generated at path: ${SVG_FILE_PATH}`);
   });
 });
